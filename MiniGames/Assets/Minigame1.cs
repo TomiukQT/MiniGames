cd88da25f802dfc291d1e1c0fbcf340cc61cb103
TomiukQT <TomiukQT@gmail.com>
2018-06-03 23:24:25 +0200

    asdf
    asdf

---
+++ MiniGames/Assets/Minigame1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame1 : MonoBehaviour {
+
+    public Transform[] cables;
+    public char[] inputs;
+ 
+    void Start()
+    {
+        cables = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            cables[i] = transform.GetChild(i);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            cables[0].GetComponent<Animation>().Play("slide");
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            cables[0].GetComponent<Animation>().Play("slideDown");
+        }
+
+
+        
+
+    }
+
+    public char GetRandomChar()
+    {
+        char ch = 'a';
+
+
+
+
+        return ch;
+    }
+}
